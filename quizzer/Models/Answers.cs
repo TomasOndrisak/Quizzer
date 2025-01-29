930c81172b599be20cc8d11489acf2e544b96464
Tomáš Ondrišák <tomas.ondrisak@gmail.com>
2025-01-29 23:03:28 +0100

    First implementation

---
+++ quizzer/Models/Answers.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Text.Json.Serialization;
+
+namespace quizzer.Models;
+public partial class Answer : ObservableObject
+{
+    public string Text { get; set; } = "";
+    public bool IsCorrect { get; set; }
+    [ObservableProperty]
+    [JsonIgnore]
+    public bool _answerIncorrect;
+    [ObservableProperty]
+    [JsonIgnore]
+    public bool _answerCorrect;
+    [ObservableProperty]
+    [JsonIgnore]
+    public bool _isSelected;
+}
+
---
+++ quizzer/Models/Question.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace quizzer.Models;
+public class Question
+{
+    public string Text { get; set; } = "";
+    public List<Answer> Answers { get; set; } = [];
+}
